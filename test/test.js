822d2b66912e52fb98d64e82b7e78f7d1ecaa678
OniOni <mathieu.c.sabourin@gmail.com>
2013-05-09 13:46:14 -0700

    Update test.

--- test/test.js
+++ test/test.js
@@ -7,18 +7,23 @@ var device = new IDevice();
 var app = path.resolve(__dirname, '../apps/TestApp.ipa'),
     appName = 'io.appium.TestApp';
 
-device.installAndWait(app, appName, function (err, success) {
-    assert.equal(null, err);
-    assert.ok(success);
-    console.log('Installed !');
 
-    device.isInstalled(appName, function (err, installed) {
+device.isInstalled(appName, function (err, installed) {
+    assert.equal(false, installed);
+
+    device.installAndWait(app, appName, function (err, success) {
 	assert.equal(null, err);
-	assert.ok(installed);
-
-	device.remove(appName, function (err) {
+	assert.ok(success);
+	console.log('Installed !');
+	
+	device.isInstalled(appName, function (err, installed) {
 	    assert.equal(null, err);
-    	    console.log('Removed !');
+	    assert.ok(installed);
+	    
+	    device.remove(appName, function (err) {
+		assert.equal(null, err);
+    		console.log('Removed !');
+	    });
 	});
     });
 });
